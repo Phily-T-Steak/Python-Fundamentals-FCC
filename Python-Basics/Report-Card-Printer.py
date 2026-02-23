3bdb3e85320da35a591b151ab2c6c80aef15518f
Phil Tobin <Philtobin18@gmail.com>
2026-02-23 07:51:30 -0700

    Adding in the Report Card Printer Workshop from FCC

---
+++ Python-Basics/Report-Card-Printer.py
@@ -0,0 +1,19 @@
+# string variable
+name = 'Alice'
+# print the variable name and type
+print(name, type(name))
+
+# boolean variable
+is_student = True
+# print the variable name and type
+print(is_student, type(is_student))
+
+# integer variable
+age = 20
+# print the variable name and type
+print(age, type(age))
+
+# float variable
+score = 80.5
+# print the variable name and type but checking that the vraiable is a specific type
+print(isinstance(score, float))
